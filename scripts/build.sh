f1f96ef2a9d9faf5048891f475d890bc24b10480
Alexandre Poirier <apoirier@kronostechnologies.com>
2016-09-23 16:13:28 -0400

    Revert "Add working build and working basic kitchen sink"
    This reverts commit 83e6b3c02c9f70ef6add680464c90f6c401959ab.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,6 @@ lib=lib
 rm -rf $lib
 rm -rf node_modules/
 npm install
-#$babel ./$src --stage 0 -d $lib
-webpack
+$babel ./$src --stage 0 -d $lib
 
 printf "\nGzip: Minified size `gzip -c $lib/index.js | wc -c | awk {'print $1/1000'}` bytes\n"
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-const cx = require('classnames');
-const findDOMNode = require('react-dom').findDOMNode;
-const React = require('react');
-const PropTypes = require('react').PropTypes;
-const Component = require('react').Component;
+import React, { PropTypes, Component } from 'react';
+import { findDOMNode } from 'react-dom';
+import cx from 'classnames';
 
 function capitalize(str) {
 	return str.charAt(0).toUpperCase() + str.substr(1);
@@ -29,204 +27,200 @@ const constants = {
 	}
 };
 
-let Slider = React.createClass({
-	getInitialState: function () {
-		return {
-			limit: 0,
-			grab: 0
-		};
-	},
+class Slider extends Component {
+	static propTypes = {
+		min: PropTypes.number,
+		max: PropTypes.number,
+		step: PropTypes.number,
+		value: PropTypes.number,
+		orientation: PropTypes.string,
+		onChange: PropTypes.func,
+		className: PropTypes.string,
+		disabled: PropTypes.bool
+	}
 
-	// Add window resize event listener here
-	componentDidMount: function () {
+	static defaultProps = {
+		min: 0,
+		max: 100,
+		step: 1,
+		value: 0,
+		orientation: 'horizontal',
+		disabled: false
+	}
+
+	state = {
+		limit: 0,
+		grab: 0,
+	}
+
+  // Add window resize event listener here
+  componentDidMount() {
 		window.addEventListener('resize', this.handleUpdate);
 		this.handleUpdate();
-	},
+  }
 
-	// remove window resize event listener here
-	componentWillUnmount: function () {
+  // remove window resize event listener here
+  componentWillUnmount() {
 		window.removeEventListener('resize', this.handleUpdate);
-	},
+  }
 
-	handleUpdate: function () {
-		let {orientation} = this.props;
-		let dimension = capitalize(constants.orientation[orientation].dimension);
-		const sliderPos = findDOMNode(this.refs.slider)['offset' + dimension];
-		const handlePos = findDOMNode(this.refs.handle)['offset' + dimension];
-		this.setState({
-			limit: sliderPos - handlePos,
-			grab: handlePos / 2,
-		});
-	},
+  handleUpdate = () => {
+  	let { orientation } = this.props;
+  	let dimension = capitalize(constants.orientation[orientation].dimension);
+  	const sliderPos = findDOMNode(this.refs.slider)['offset' + dimension];
+  	const handlePos = findDOMNode(this.refs.handle)['offset' + dimension]
+  	this.setState({
+  		limit: sliderPos - handlePos,
+  		grab: handlePos / 2,
+  	});
+  }
 
-	handleStart: function () {
-		document.addEventListener('mousemove', this.handleDrag);
-		document.addEventListener('mouseup', this.handleEnd);
-	},
+  handleStart = () => {
+  	document.addEventListener('mousemove', this.handleDrag);
+  	document.addEventListener('mouseup', this.handleEnd);
+  }
 
-	handleDrag: function (e) {
-		this.handleNoop(e);
-		let value, {onChange} = this.props;
-		if (!onChange) {
-			console.warn('No onChange was specified for selected slider.')
-		}
+  handleDrag = (e) => {
+  	this.handleNoop(e);
+  	let value, { onChange } = this.props;
+  	if (!onChange) return;
 
-		value = this.position(e);
-		onChange && onChange(value);
-	},
+  	value = this.position(e);
+  	onChange && onChange(value);
+  }
 
-	handleEnd: function (e) {
-		if (this.props.onMouseUp) {
-			let value = this.position(e);
-			this.props.onMouseUp(value);
-		}
+  handleEnd = (e) => {
+	if(this.props.onMouseUp){
+		let value = this.position(e);
+		this.props.onMouseUp(value);
+	}
 
-		document.removeEventListener('mousemove', this.handleDrag);
-		document.removeEventListener('mouseup', this.handleEnd);
-	},
+  	document.removeEventListener('mousemove', this.handleDrag);
+  	document.removeEventListener('mouseup', this.handleEnd);
+  }
 
-	handleNoop: function (e) {
-		e.stopPropagation();
-		e.preventDefault();
-	},
+  handleNoop = (e) => {
+  	e.stopPropagation();
+  	e.preventDefault();
+  }
 
-	handleSliderMouseUp: function (e) {
-		if (this.props.onSliderMouseUp) {
-			let value = this.position(e);
-			this.props.onSliderMouseUp(value);
-		}
-	},
+  handleSliderMouseUp = (e) =>{
+	  if(this.props.onSliderMouseUp){
+		  let value = this.position(e);
+		  this.props.onSliderMouseUp(value);
+	  }
+  }
 
-	getPositionFromValue: function (value) {
-		let percentage, pos;
-		let {limit} = this.state;
-		let {min, max} = this.props;
-		percentage = (value - min) / (max - min);
-		pos = Math.round(percentage * limit);
+  getPositionFromValue = (value) => {
+  	let percentage, pos;
+  	let { limit } = this.state;
+  	let { min, max } = this.props;
+  	percentage = (value - min) / (max - min);
+  	pos = Math.round(percentage * limit);
 
-		return pos;
-	},
+  	return pos;
+  }
 
-	getValueFromPosition: function (pos) {
-		let percentage, value;
-		let {limit} = this.state;
-		let {orientation, min, max, step} = this.props;
-		percentage = (maxmin(pos, 0, limit) / (limit || 1));
+  getValueFromPosition = (pos) => {
+  	let percentage, value;
+  	let { limit } = this.state;
+  	let { orientation, min, max, step } = this.props;
+  	percentage = (maxmin(pos, 0, limit) / (limit || 1));
 
-		if (orientation === 'horizontal') {
-			value = step * Math.round(percentage * (max - min) / step) + min;
-		} else {
-			value = max - (step * Math.round(percentage * (max - min) / step) + min);
-		}
+  	if (orientation === 'horizontal') {
+  		value = step * Math.round(percentage * (max - min) / step) + min;
+  	} else {
+  		value = max - (step * Math.round(percentage * (max - min) / step) + min);
+  	}
 
-		return value;
-	},
+  	return value;
+  }
 
-	position: function (e) {
-		let pos, value, {grab} = this.state;
-		let {orientation} = this.props;
-		const node = findDOMNode(this.refs.slider);
-		const coordinateStyle = constants.orientation[orientation].coordinate;
-		const directionStyle = constants.orientation[orientation].direction;
-		const coordinate = !e.touches
+  position = (e) => {
+  	let pos, value, { grab } = this.state;
+  	let { orientation } = this.props;
+  	const node = findDOMNode(this.refs.slider);
+  	const coordinateStyle = constants.orientation[orientation].coordinate;
+  	const directionStyle = constants.orientation[orientation].direction;
+  	const coordinate = !e.touches
 			? e['client' + capitalize(coordinateStyle)]
 			: e.touches[0]['client' + capitalize(coordinateStyle)];
-		const direction = node.getBoundingClientRect()[directionStyle];
+  	const direction = node.getBoundingClientRect()[directionStyle];
 
-		pos = coordinate - direction - grab;
-		value = this.getValueFromPosition(pos);
+  	pos = coordinate - direction - grab;
+  	value = this.getValueFromPosition(pos);
 
-		return value;
-	},
+  	return value;
+  }
 
-	coordinates: function (pos) {
-		let value, fillPos, handlePos;
-		let {limit, grab} = this.state;
-		let {orientation} = this.props;
+  coordinates = (pos) => {
+  	let value, fillPos, handlePos;
+  	let { limit, grab } = this.state;
+  	let { orientation } = this.props;
 
-		value = this.getValueFromPosition(pos);
-		handlePos = this.getPositionFromValue(value);
+  	value = this.getValueFromPosition(pos);
+  	handlePos = this.getPositionFromValue(value);
 
-		if (orientation === 'horizontal') {
-			fillPos = handlePos + grab;
-		} else {
-			fillPos = limit - handlePos + grab;
-		}
-		/*
-		 The fillReturn variable is manipulated to cover the edge cases.
-		 If this.props.fill is zero, the fillReturn has to be zero.
-		 Between zero and this.props.max, the grab value is added to be accurate with the handle position.
-		 If this.props.fill equals this.props.max, the range slider has to be completely filled, which explains the " + (2 * grab)".
-		 */
-		let fillReturn = 0;
-		if (this.props.fill > 0 && this.props.fill < this.props.max) {
-			fillReturn = this.getPositionFromValue(this.props.fill) + grab;
-		} else if (this.props.fill == this.props.max) {
-			fillReturn = this.getPositionFromValue(this.props.fill) + ( 2 * grab);
-		}
+  	if (orientation === 'horizontal') {
+  		fillPos = handlePos + grab;
+  	} else {
+  		fillPos = limit - handlePos + grab;
+  	}
+  	/*
+  	The fillReturn variable is manipulated to cover the edge cases.
+  	If this.props.fill is zero, the fillReturn has to be zero.
+  	Between zero and this.props.max, the grab value is added to be accurate with the handle position.
+  	If this.props.fill equals this.props.max, the range slider has to be completely filled, which explains the " + (2 * grab)".
+  	 */
+	  let fillReturn = 0;
+	  if (this.props.fill > 0 && this.props.fill < this.props.max) {
+		  fillReturn = this.getPositionFromValue(this.props.fill) + grab;
+	  } else if(this.props.fill == this.props.max) {
+		  fillReturn = this.getPositionFromValue(this.props.fill) + ( 2 * grab);
+	  }
 
-		return {
-			fill: (this.props.fill >= 0) ? (fillReturn) : fillPos,
-			handle: handlePos,
-		};
-	},
+  	return {
+  		fill: (this.props.fill >= 0) ? (fillReturn) : fillPos,
+  		handle: handlePos,
+  	};
+  }
 
-	render: function () {
-		let dimension, direction, position, coords, fillStyle, handleStyle;
-		let {value, orientation, className} = this.props;
+  render() {
+  	let dimension, direction, position, coords, fillStyle, handleStyle;
+  	let { value, orientation, className } = this.props;
 
-		dimension = constants.orientation[orientation].dimension;
-		direction = constants.orientation[orientation].direction;
+  	dimension = constants.orientation[orientation].dimension;
+  	direction = constants.orientation[orientation].direction;
 
-		position = this.getPositionFromValue(value);
-		coords = this.coordinates(position);
+  	position = this.getPositionFromValue(value);
+  	coords = this.coordinates(position);
 
-		fillStyle = {[dimension]: `${coords.fill}px`};
-		handleStyle = {[direction]: `${coords.handle}px`};
+  	fillStyle = {[dimension]: `${coords.fill}px`};
+  	handleStyle = {[direction]: `${coords.handle}px`};
 
-		return (
-			<div
-				ref="slider"
-				className={cx('rangeslider ', 'rangeslider-' + orientation, className)}
-				onMouseDown={this.props.disabled ? function(){} : this.handleDrag}
-				onClick={this.props.disabled ? function(){} :this.handleNoop}
-				onMouseUp={this.props.disabled ? function(){} :this.handleSliderMouseUp}
-				disabled={this.props.disabled}
-			>
-				<div
-					ref="fill"
-					className="rangeslider__fill"
-					style={fillStyle}/>
-				<div
-					ref="handle"
-					className="rangeslider__handle"
+  	return (
+  		<div
+	  		ref="slider"
+	  		className={cx('rangeslider ', 'rangeslider-' + orientation, className)}
+	  		onMouseDown={this.props.disabled ? function(){} : this.handleDrag}
+	  		onClick={this.props.disabled ? function(){} :this.handleNoop}
+	        onMouseUp={this.props.disabled ? function(){} :this.handleSliderMouseUp}
+	        disabled={this.props.disabled}
+	    >
+	  		<div
+		  		ref="fill"
+		  		className="rangeslider__fill"
+		  		style={fillStyle} />
+	  		<div
+		  		ref="handle"
+		  		className="rangeslider__handle"
 					onMouseDown={this.props.disabled ? function(){} :this.handleStart}
 					onTouchMove={this.props.disabled ? function(){} :this.handleDrag}
 					onClick={this.props.disabled ? function(){} :this.handleNoop}
-					style={handleStyle}/>
-			</div>
+		  		style={handleStyle} />
+  		</div>
 		);
-	}
-});
+  }
+}
 
-Slider.propTypes = {
-	min: PropTypes.number,
-	max: PropTypes.number,
-	step: PropTypes.number,
-	value: PropTypes.number,
-	orientation: PropTypes.string,
-	onChange: PropTypes.func,
-	className: PropTypes.string,
-	disabled: PropTypes.bool
-};
-
-Slider.defaultProps = {
-	min: 0,
-	max: 100,
-	step: 1,
-	value: 0,
-	orientation: 'horizontal',
-	disabled: false
-};
-
-module.exports = Slider;
+export default Slider;
